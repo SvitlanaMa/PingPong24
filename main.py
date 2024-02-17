f2cbd6938cf1f627fa08017496cd673070c182d3
Svitlana <fortestingdev2022@gmail.com>
2024-02-17 14:31:58 +0200

    window create

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+pygame.init()
+
+win_w = 700
+win_h = 500
+
+FPS = 40
+
+window = pygame.display.set_mode((win_w, win_h))
+
+clock = pygame.time.Clock()
+
+game = True
+while game:
+    window.fill((0, 255, 0))
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    pygame.display.update()
+    clock.tick(FPS)
